bb1253740b1c00bb21e50d899b6262a0d591cda4
Falwizor <falwizor@gmail.com>
2025-07-23 23:19:44 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1569,9 +1569,9 @@ function RayfieldLibrary:CreateWindow(Settings)
 	if Rayfield:FindFirstChild('Loading') then
 		if getgenv and not getgenv().rayfieldCached then
 			Rayfield.Enabled = true
-			Rayfield.Loading.Visible = true
+			Rayfield.Loading.Visible = false
 
-			task.wait(5.4)
+			task.wait(1.4)
 			Rayfield.Loading.Visible = false
 		end
 	end
